720bb7cdf1f1f27f935c7ccec3770d68b1a93f3f
George Felix <gfelixc@gmail.com>
2017-07-15 12:10:33 +0200

    Safe refactor Uui data type

--- src/DataMiner/Domain/DataType/Uuid.php
+++ src/DataMiner/Domain/DataType/Uuid.php
@@ -5,19 +5,29 @@ namespace Mpwar\DataMiner\Domain\DataType;
 use InvalidArgumentException;
 use Ramsey\Uuid\Uuid as RamseyUuid;
 
-class Uuid
+abstract class Uuid
 {
     private $value;
 
     private function __construct($value)
     {
-        if (!RamseyUuid::isValid($value)) {
-            throw new InvalidArgumentException();
-        }
+        $this->setValue($value);
+    }
+
+    private function setValue($value): void
+    {
+        $this->isValid($value);
         $this->value = $value;
     }
 
-    public static function new()
+    private function isValid($value): void
+    {
+        if (!RamseyUuid::isValid($value)) {
+            throw new InvalidArgumentException();
+        }
+    }
+
+    public static function new(): self
     {
         $ramseyUuid = RamseyUuid::uuid4()
                                 ->toString();
@@ -25,18 +35,18 @@ class Uuid
         return new static($ramseyUuid);
     }
 
-    public static function fromString(string $uuid)
+    public static function fromString(string $uuid): self
     {
         return new static($uuid);
+    }
+
+    public function __toString(): string
+    {
+        return $this->value();
     }
 
     public function value(): string
     {
         return $this->value;
-    }
-
-    public function __toString()
-    {
-        return $this->value();
     }
 }
